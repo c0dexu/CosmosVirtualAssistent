5c62695ba8cd770c89e6faa1e6516e9b4f0f39a2
Cicioc Vlad <ciciocvlad@gmail.com>
2023-12-09 18:34:25 +0200

    swapped to flask

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-from typing import Union
-
-from fastapi import FastAPI
-
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 from chat_bot import chat
 
-app = FastAPI()
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
+
+@app.route("/", methods=['GET'])
+def hello_world():
+    response = jsonify({'response': 'Hello, World!'})
+    return response
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.route("/chat", methods=['GET'])
+def read_item():
+    message = chat(request.args.get('message'))
+    response = jsonify({'response': message})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 
-@app.get("/chat")
-def read_item(message: str):
-    return chat(message)
+if __name__ == "__main__":
+    app.run(debug=True)
